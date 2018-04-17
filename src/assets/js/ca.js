e8f85686ad1bfbcc4cf9361fc5a528d5b543cbeb
adon988 <adon988@gmail.com>
2018-04-17 15:37:15 +0800

    'startbuildcapart'

---
+++ src/assets/js/ca.js
@@ -0,0 +1,327 @@
+﻿
+//讀取憑證資訊宣告
+var AllCertInfo = {
+    sn: '',//憑證序號
+    cn: '',//憑證主旨
+    ntb: '',
+    nta: '',
+    issdn: '',
+    subdn: '',
+    certfinger: '',
+    rawdata: '',//明文
+    certtype: '',//E:台網;C:中華
+    certb64: '',//密文
+    verify_Code: '',
+    verify_P7: '',
+    verrify_Msg: '',
+    code: '',
+    msg: '',
+    action: 'NO'
+};
+var VerifyInfo = {
+    verify_Code: '',
+    verify_P7: '',
+    verrify_Msg: ''
+}
+
+var VerifyResult = '';
+var g_CN = '';
+var g_RawData = '';
+
+//2016-11-22 新增各電子前台傳入idno, token
+var Front = {
+    idno: '',
+    token: '',
+    client: '',
+    source: ''
+};
+//2016-11-22 新增判斷有無登入過
+var check_login = '';
+
+//2017-01-11 postdata
+var OtherDomain = {
+    idno: '',
+    token: '',
+    backurl: '',
+    client: '',
+    source: 'web'
+};
+//--------unnesscessary now--------//
+function btnGetVersionOnClick() {
+
+    twcaLib.GetVersion("_retGetVersion");
+
+}
+function _retGetVersion(code, msg, token, data) {
+    if (data != "") {
+        data = JSON.parse(data).version;
+    }
+
+    var oErrCode = $f("GetVersion_errCode");
+    oErrCode.innerHTML = "錯誤代碼: " + code + "\n"
+            + "錯誤訊息: " + msg + "\n"
+            + "元件版本: " + data + "\n";
+}
+//--------unnesscessary now--------//
+var checkTestVar=0;
+//檢查有無憑證function
+function checkBrowserCert_PhoneWeb(id, token, url) {
+    console.log('checkTestVar is '+checkTestVar);
+    console.log("checkBrowserCert_PhoneWeb ： id = " + id + ",token = " + token + ", url = " + url);
+    //穿透使用開始↓檢查有無登入權限，並將資訊寫入session
+    OtherDomain.idno = id;
+    OtherDomain.token = token;
+    OtherDomain.backurl = url;
+   
+    //穿透使用結束↑
+    //var GotoCheck = function () {
+    var errCode = -1;
+    //暫時註解formal↓
+    var oFilter = "//S_CN=" + AllCertInfo.cn + ",S_OU=The Capital Group,S_OU=RA-TheCapital,S_O=Certification Service Provider,S_O=TaiCA Secure CA,S_C=TW//";  //正式環境用
+    //testing
+    //var oFilter = "//S_CN=" + AllCertInfo.cn + ",S_OU=The Capital Group,S_OU=RA-TheCapitalT,S_O=Certificate Service Provider - Evaluation Only,S_O=TaiCA Secure CA - Evaluation Only,S_C=TW//";  //測試環境用
+    //testing 2016/11/12
+    //var oFilter = "//S_C=TW//";
+    var oPassword = "";
+    var oIsPasswordNull = "";
+    var dDwFlagCheckCert = "0x00001000";
+
+    var dwFlags = 0x04 | 0x08 | dDwFlagCheckCert;
+
+    var userPin = oPassword;
+    userPin = null;
+    console.log('ready to twcaLib');
+    do {
+        console.log('continue to twcaLib');
+        twcaLib.SelectSignerEx(oFilter, "", "", userPin, "", dwFlags, 0, null, "_retSelectSignerOnClicked_PhoneWeb");
+    } while (false);
+    //};
+
+}
+//輸入憑證匯入密碼function
+function pwdBrowserCert() {
+    var errCode = -1;
+    //暫時註解↓
+    var oFilter = "//S_CN=" + AllCertInfo.cn + ",S_OU=The Capital Group,S_OU=RA-TheCapital,S_O=Certification Service Provider,S_O=TaiCA Secure CA,S_C=TW//";     //正式環境用
+    //testing 2016/11/12
+    //var oFilter = "//S_C=TW//";
+    //testing
+    //console.log(AllCertInfo.cn);
+    //var oFilter = "//S_CN=" + AllCertInfo.cn + ",S_OU=The Capital Group,S_OU=RA-TheCapitalT,S_O=Certificate Service Provider - Evaluation Only,S_O=TaiCA Secure CA - Evaluation Only,S_C=TW//";   //測試環境用
+    var oPassword = "";
+    var oIsPasswordNull = "";
+    var dDwFlagCheckCert = "0x00000000";
+    var dwFlags = 0x04 | 0x08 | dDwFlagCheckCert;
+    var userPin = oPassword;
+    userPin = null;
+    do {
+        twcaLib.SelectSignerEx(oFilter, "", "", userPin, "", dwFlags, 0, null, "_retSelectSignerOnClicked_PhoneWeb");
+    } while (false);
+}
+
+function _retSelectSignerOnClicked_PhoneWeb(code, msg, token, data) {
+    console.log("_retSelectSignerOnClicked_PhoneWeb" + code);
+
+    console.log({"note":"retselectsigner" ,"code":code, "msg": msg, "token": token, "data": data});
+    var CertInfo = "";
+
+    if (data != "") {
+        data = JSON.parse(data);
+    }
+    // var oErrCode = $f("SelectSigner_errCode");
+
+
+    if (code == "5005") {
+        //參數錯誤
+    }
+    if (code == "5010") {  
+        console.log('無有效TWID憑證');      
+        var fn_formatdiv = function (callback) {
+            console.log("目前此裝置無有效TWID憑證，將導引到TWID憑證APP作業，若您尚未下載TWID APP，" +
+                 "將自動導至Store/Play商店，請點選TWID APP進行安裝即可，並請於安裝完成後，" +
+                 "關閉TWID APP，回到CBAS交易系統頁面進行後續操作，謝謝。")
+            var formatdiv = '';
+            formatdiv += '<form action="https://goplus.capital.com.tw/CapitalCA/login.aspx" method="post" name="form1" id="form1">';  //正式環境
+            //formatdiv += '<form action="https://dev-goplus.capital.com.tw/CapitalCA/login.aspx" method="post" name="form1" id="form1">';  //測試環境
+            formatdiv += '<input type="hidden" name="idno" id="idno" value="' + OtherDomain.idno + '" />';
+            formatdiv += '<input type="hidden" name="token" id="token" value="' + OtherDomain.token + '" />';
+            formatdiv += '<input type="hidden" name="backurl" id="backurl" value="' + OtherDomain.backurl + '" />';
+            //formatdiv += '<input type="submit" value="Send data">';
+            formatdiv += '</form>';
+            $("body").append(formatdiv);
+            callback();
+        };
+        var fn_submit = function () {
+            $('#form1').submit();
+        };
+        fn_formatdiv(fn_submit);
+
+        //OtherDomain.idno = id;
+        //OtherDomain.token = token;
+        //OtherDomain.backurl = url;
+    }
+    if (code == "5070") {
+        //使用者按下取消
+        return false;
+    }
+    if (code == "5071") {
+        //憑證密碼錯誤
+        console.log("憑證匯入密碼錯誤")
+        return false;
+    }
+    if (code == "5112") {
+        //讀取憑證失敗
+    }
+    if (code == "5001") {
+        //一般錯誤
+    }
+
+    for (var key in data) {
+        if (code = "0") {
+            switch (key) {
+                case "sn":
+                    AllCertInfo.sn = data[key];
+                    if (data[key].length <= '8') {
+                        AllCertInfo.certtype = 'E';
+                    }
+                    else {
+                        AllCertInfo.certtype = 'C';
+                    }
+                    break;
+                case "cn":
+                    AllCertInfo.cn = data[key];
+                    break;
+                case "ntb":
+                    AllCertInfo.ntb = data[key];
+                    break;
+                case "nta":
+                    AllCertInfo.nta = data[key];
+                    break;
+                case "issdn":
+                    AllCertInfo.issdn = data[key];
+                    break;
+                case "subdn":
+                    AllCertInfo.subdn = data[key];
+                    break;
+                case "certfinger":
+                    AllCertInfo.certfinger = data[key];
+                    break;
+                case "certb64":
+                    AllCertInfo.certb64 = data[key];
+                    break;
+                default:
+                    break;
+            }
+        }
+        CertInfo += key + ": " + data[key] + "\n";
+    }
+
+
+
+    if (code == "0") {
+        btnSignPkcs7OnClick_PhoneWeb();
+    }
+}
+
+
+function btnSignPkcs7OnClick_PhoneWeb() {
+    //console.log("btnSignPkcs7OnClick_PhoneWeb");
+    do {
+        var dwFlags = 0x0000 | 0x0001 | 0x0000;
+        twcaLib.SignPkcs7(AllCertInfo.rawdata, dwFlags, "_retSignPkcs7OnClick_PhoneWeb");
+    } while (false);
+}
+function _retSignPkcs7OnClick_PhoneWeb(code, msg, token, data) {
+
+    if (data != "") {
+        data = JSON.parse(data).signature;
+    }
+    var VerifyInfo = {
+        verify_Code: '',
+        verify_P7: '',
+        verrify_Msg: ''
+    }
+    VerifyInfo.verify_Code = code;
+    if (code == "0") {
+        AllCertInfo.action = 'YES';
+        AllCertInfo.verify_P7 = data;
+        AllCertInfo.verrify_Msg = msg;
+    }
+    if (code == "5005") {
+        //參數錯誤
+        AllCertInfo.verify_P7 = "";
+        AllCertInfo.verrify_Msg = msg;
+    }
+    if (code == "5067") {
+        //執行簽章時，尚未選擇簽章憑證
+        AllCertInfo.verify_P7 = "";
+        AllCertInfo.verrify_Msg = msg;
+        pwdBrowserCert();
+
+    }
+    if (code == "5061") {
+        //簽章失敗
+        AllCertInfo.verify_P7 = "";
+        AllCertInfo.verrify_Msg = msg;
+    }
+    var oErrCode = $f("SignPkcs7_signValue");
+}
+function btnSignPkcs1OnClick() {
+
+    var oPlainText = $f("SignPkcs1_plainText");
+    var oFlag_Out = $f("SignPkcs1_FLAG_Out");
+    var oFlag_Hash = $f("SignPkcs1_FLAG_Hash");
+
+    do {
+        var dwFlags = 0x0100;
+        twcaLib.SignPkcs1(oPlainText.value, dwFlags, "_retSignPkcs1OnClick_PhoneWeb");
+
+    } while (false);
+
+}
+function _retSignPkcs1OnClick_PhoneWeb(code, msg, token, data) {
+    var oErrCode = $f("SignPkcs1_signValue");
+    oErrCode.innerHTML = "錯誤代碼: " + code + "\n"
+            + "錯誤訊息: " + msg + "\n"
+            + "回傳資訊: " + data + "\n";
+}
+function btnResetKeyOnClick() {
+
+    twcaLib.ResetKey("_retResetKey");
+
+}
+function _retResetKey(code, msg, token, data) {
+    var oErrCode = $f("ResetKey_errCode");
+    oErrCode.innerHTML = "錯誤代碼: " + code + "\n"
+            + "錯誤訊息: " + msg + "\n";
+}
+function btnNewWindow() {
+
+    window.open("./signTest_childWindow.htm", 'mywin', 'scrollbars=yes,width=500,height=400');
+
+}
+function SelectSignerPasswordIsNullOnClicked() {
+
+    var oIsNull = $f("SelectSigner_password_isNull");
+    var oPassword = $f("SelectSigner_password");
+
+    if (oIsNull.checked) {
+        oPassword.disabled = true;
+    } else {
+        oPassword.disabled = false;
+    }
+
+}
+//--------unnesscessary now--------//
+
+function $f(tagId) {
+    if (document.getElementById(tagId)) {
+        return document.getElementById(tagId);
+    } else {
+        return null;
+    }
+}
+function downloadTCEMAP() {
+    location.href = 'CapitalAP/TCEM.exe';  //正式環境
+    //location.href = 'CapitalAP/TCEM(test).exe';   //測試環境
+}
--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController, LoadingController } from 'ionic-angular';
 
 declare var $;
+declare var AllCertInfo;
+declare var checkBrowserCert_PhoneWeb;
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
@@ -16,11 +18,12 @@ export class ContactPage {
   password='1111';
   CBAS_URL_GW_PORTAL = "https://dev-cbas.capital.com.tw/ExtProduct/Program/Portal/adam_test/";
   CBAS_URL_GW_PORTAL_IP = "https://dev-cbas.capital.com.tw/ExtProduct/Program/Portal/adam_test/getIP.aspx";
+  CBAS_URL = "https://dev-cbas.capital.com.tw/ExtProduct/Program/CBAS/adam_test/"
   urlcode = "QG8RQS6FC7";
   check_results;
   ip_data;
 
-  //login check
+  //login
   Login(){
       this.check_results='';
       this.ip_data='';
@@ -28,7 +31,8 @@ export class ContactPage {
         content: "驗證中...",
       });
       loader.present();
-      setTimeout(() => {let ng2= this;
+      setTimeout(() => {
+        let ng2= this;
         //Get IP
         $.getJSON(ng2.CBAS_URL_GW_PORTAL_IP).done(function(data){
           console.log(data);
@@ -38,6 +42,17 @@ export class ContactPage {
             console.log(data);
             ng2.ip_data = get_ip;
             ng2.check_results = data;//JSON.stringify(data);
+
+            let token = data.CAStatus.split(";")[1].split("{")[1].split("}")[0];
+            let acno = data.Acno;
+
+            if (ng2.id.toUpperCase() == "SELF0082") {
+              console.log('營業員 自動登入');
+            }else{
+              console.log('一般使用者登入，需進入CA流程');
+              ng2.certificate_authority({type:'signIn', id: ng2.id.toUpperCase(), rawdata: "srvgateway_Cert_S0001_"+acno, ip: get_ip, acno:acno, token:token });
+            }
+            
           }).fail(function(jqXHR, textStatus) {
             alert( "Request failed: " + textStatus );
           }).always(function() {
@@ -48,5 +63,93 @@ export class ContactPage {
           alert( "Request failed: " + textStatus );
         });
       }, 3000);
-  }
+  }//Login()
+
+  
+  deviceUserAgent='----';
+  //Certificate Authority
+  certificate_authority(obj){
+    let ng2 = this;
+    console.log({ 
+      "note": "1",
+      "idno": obj.id,
+      "trantype": "78", 
+      "certtype": AllCertInfo.certtype, //AllCertInfo.certtype
+      "cn": obj.id,
+      "certid": AllCertInfo.sn,//AllCertInfo.sn 
+      "b64p7sig": AllCertInfo.verify_P7, //AllCertInfo.verify_P7
+      "rawdata": obj.rawdata, 
+      "clientip":obj.ip, 
+      "source": "web"});
+    //Agent check - Mobile Web
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+      ng2.deviceUserAgent = 'Agent: mobile web'+navigator.userAgent;
+        let Url_data = "http://dev-ex.capital.com.tw/ExtProduct/Program/CBAS/Index_20180102.html?ID=" + obj.id + "&Acno=" + obj.acno + "&Token=" + obj.token;
+        
+        checkBrowserCert_PhoneWeb(obj.id, obj.token, Url_data);
+
+
+        console.log({ 
+          "note":"2",
+          "idno": obj.id,
+          "trantype": "78", 
+          "certtype": AllCertInfo.certtype, //AllCertInfo.certtype
+          "cn": obj.id,
+          "certid": AllCertInfo.sn,//AllCertInfo.sn 
+          "b64p7sig": AllCertInfo.verify_P7, //AllCertInfo.verify_P7
+          "rawdata": obj.rawdata, 
+          "clientip":obj.ip, 
+          "source": "web"});
+      // $.ajax({
+      //     url: ng2.CBAS_URL + 'S0001.ashx',
+      //     type: "POST",
+      //     async: true,
+      //     data: { 
+      //               "idno": obj.id,
+      //               "trantype": "78", 
+      //               "certtype": AllCertInfo.certtype, //AllCertInfo.certtype
+      //               "cn": obj.id,
+      //               "certid": AllCertInfo.sn,//AllCertInfo.sn 
+      //               "b64p7sig": AllCertInfo.verify_P7, //AllCertInfo.verify_P7
+      //               "rawdata": obj.rawdata, 
+      //               "clientip":obj.ip, 
+      //               "source": "web"},
+      //     success: function (data) {
+      //         console.log('完成');
+      //         console.log(data);  
+            // //alert(data);
+              // var ary = data.split(":")[1].split(",");
+              // var ary_msg = data.split(":")[2].split("}");
+              // if (ary[0] == '"000"') {
+              //     //alert("PhoneWeb_SECCESS");
+              //     // window.location.href = urlPortal + ID.toUpperCase() + "&Acno=" + Acno + "&Token=" + Token;
+              //     console.log('phoneweb success');
+              // }
+              // else {
+              //     console.log('other success');
+              //     // if (Source == "ios" || Source == "android") {
+              //     //     alert(ary_msg[0]);
+              //     // }
+              //     // else {
+              //     //     alert(ary_msg[0]);
+              //     // }
+              // }
+          // },
+      //     error: function (xhr) {
+
+      //         console.log('error happened');
+      //         console.log(xhr);
+      //         // if (Source == "ios" || Source == "android") {
+      //         //     SystemMessage(xhr.statusText);
+      //         // }
+      //         // else {
+      //         //     alert(xhr.statusText);
+      //         // }
+      //     }
+      // });
+    }else{
+      ng2.deviceUserAgent = 'Agent: app'+navigator.userAgent;
+    }
+  }//certificate_authority()
+  
 }
